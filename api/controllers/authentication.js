302c5be0adf5fc7bb22bdf6fa1717556b85f55ef
Jadir Junior <jadirjunior@trendsitsolutions.com.br>
2017-04-06 15:53:42 -0300

    started boilerplate

---
+++ api/controllers/authentication.js
@@ -0,0 +1,50 @@
+const passport = require('passport');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
+module.exports.register = (req, res) => {
+  if(!req.body.name || !req.body.email || !req.body.password) {
+    res.status(400),json({"message": "All fields required"})
+    return;
+  }
+
+  const user = new User();
+
+  user.name = req.body.name;
+  user.email = req.body.email;
+
+  user.setPassword(req.body.password);
+
+  user.save(err => {
+    let token;
+    if (err) {
+      res.status(404).json(err);
+    } else {
+      token = user.generateJwt();
+      res.status(200).json({"token": token})
+    }
+  });
+};
+
+module.exports.login = (req, res) => {
+  if(!req.body.email || !req.body.password) {
+    res.status(400).json({"message": "All fields required"});
+    return;
+  }
+
+  passport.authenticate('local', (err, user, info) => {
+    let token;
+
+    if (err) {
+      res.status(404).json(err);
+      return;
+    }
+
+    if (user) {
+      token = user.generateJwt();
+      res.status(200).json({"token": token});
+    } else {
+      res.status(401).json(info);
+    }
+  })(req, res);
+};
---
+++ api/models/db.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+const readLine = require('readline');
+
+let dbURI = 'mongodb://localhost/Loc8r';
+
+if(process.env.NODE_ENV === 'production') {
+  dbURI = process.env.MONGOLAB_URI;
+}
+
+mongoose.connect(dbURI);
+
+// Monitoring the Mongoose connection events
+
+if(process.platform === 'win32') {
+  let rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.on("SIGINT", () => {
+    process.emit("SIGINT");
+  });
+  rl.on("SIGUSR2", () => {
+    process.emit("SIGUSR2");
+  });
+  rl.on('SIGTERM', () => {
+    process.emit('SIGTERM');
+  });
+}
+
+const gracefulShutdown = (msg, callback) => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose disconnected through ' + msg);
+    callback();
+  });
+};
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to ' + dbURI);
+});
+
+mongoose.connection.on('error', err => {
+  console.log('Mongoose connected error ' + err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
+
+process.once('SIGUSR2', () => {
+  gracefulShutdown('nodemon restart', () => {
+    process.kill(process.id, 'SIGUSR2');
+  });
+});
+
+process.on('SIGINT', () => {
+  gracefulShutdown('app termination', () => {
+    process.exit(0);
+  });
+});
+
+process.on('SIGTERM', () => {
+  gracefulShutdown('Heroku app shutdown', () => {
+    process.exit(0);
+  });
+});
+
+// put your models down in the code
+require('./users');
